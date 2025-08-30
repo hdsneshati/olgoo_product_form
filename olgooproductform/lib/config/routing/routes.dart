f0e36442d7d7573b4ddb6e4265d9f3bdc960000f
hdsneshati <hadisneshati1384@gmail.com>
2025-08-31 00:32:48 +0330

    add rourer

--- olgooproductform/lib/config/routing/routes.dart
+++ olgooproductform/lib/config/routing/routes.dart
@@ -1,39 +1,20 @@
-/*import 'package:flutter/material.dart';
-import 'package:flutter_olgoo_product/feature/presentation/authentication/screens/login.screen.dart';
-import 'package:flutter_olgoo_product/feature/presentation/authentication/screens/otp.screen.dart';
-import 'package:flutter_olgoo_product/feature/presentation/authentication/screens/signup.step1.screen.dart';
-import 'package:flutter_olgoo_product/feature/presentation/authentication/screens/signup_stap2_details_screen.dart';
-import 'package:flutter_olgoo_product/feature/presentation/dashboard/screens/signup_step3_product.dart';
-import 'package:flutter_olgoo_product/feature/presentation/dashboard/screens/signup_step4_order.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:olgooproductform/feature/presentation/dashboard/screens/dashboard_screen.dart';
+import 'package:olgooproductform/feature/presentation/dashboard/screens/signup_step3_product.dart';
+import 'package:olgooproductform/feature/presentation/dashboard/screens/signup_step4_order.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 final GoRouter routs =
-    GoRouter(navigatorKey: navigatorKey, initialLocation: "/signupstep4order", routes: [
+    GoRouter(navigatorKey: navigatorKey, initialLocation: "/dashboard", routes: [
  
  
  
  
-  GoRoute(
-    path: '/login',
-    name: "/login",
-    builder: (context, state) => const LoginScreen(),
-  ),
-   GoRoute(
-      path: '/otp',
-      name: "/otp",
-      builder: (context, state) {
-        final String userPhoneNumber = state.extra as String;
-        return OtpScreen(
-         userPhoneNumber: userPhoneNumber,
-       );
-      }),
-  GoRoute(
-    path: '/signup',
-    name: "/signup",
-    builder: (context, state) => SignupScreen(),
-  ),
+ 
+   
+  
    GoRoute(
     path: '/signupstep3product',
     name: "/signupstep3product",
@@ -45,9 +26,9 @@ final GoRouter routs =
     builder: (context, state) => SignupStep4Order(),
   ),
    GoRoute(
-    path: '/details',
-    name: "/details",
-    builder: (context, state) => SignupDetailsScreen(),
-),
+    path: '/dashboard',
+    name: "/dashboard",
+    builder: (context, state) => DashboardScreen(),
+  ),
   
-]);*/
+]);
