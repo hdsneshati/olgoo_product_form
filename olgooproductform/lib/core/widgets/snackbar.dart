3b7bee4da263030f4642edf38b3fda60a0a3fe37
hdsneshati <hadisneshati1384@gmail.com>
2025-08-31 00:34:44 +0330

    add snakbar

---
+++ olgooproductform/lib/core/widgets/snackbar.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:olgooproductform/config/asset/theme/color_pallet.dart';
+import 'package:toastification/toastification.dart';
+
+class SnackBars {
+  static successSnackBar(
+      BuildContext context, String title, String decription) {
+    toastification.show(
+      context: context,
+      type: ToastificationType.success,
+      style: ToastificationStyle.flatColored,
+ title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: "dana",
+            color: ColorPallet.lightColorScheme.onSurface,
+            fontSize: 14,
+            fontWeight: FontWeight.w600),
+      ),
+      description: Text(decription,style: TextStyle(
+            fontFamily: "dana",
+            color: ColorPallet.lightColorScheme.onSurface,
+            fontSize: 12,
+            fontWeight: FontWeight.w400),),
+      alignment: Alignment.bottomCenter,
+      autoCloseDuration: const Duration(seconds: 4),
+      primaryColor: Theme.of(context).colorScheme.primary,
+      foregroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      borderRadius: BorderRadius.circular(12.0),
+      direction: TextDirection.rtl,
+    );
+  }
+  static infoSnackBar(
+      BuildContext context, String title, String decription) {
+    toastification.show(
+      context: context,
+      type: ToastificationType.info,
+      style: ToastificationStyle.flatColored,
+ title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: "dana",
+            color: ColorPallet.lightColorScheme.onSurface,
+            fontSize: 14,
+            fontWeight: FontWeight.w600),
+      ),
+      description: Text(decription,style: TextStyle(
+            fontFamily: "dana",
+            color: ColorPallet.lightColorScheme.onSurface,
+            fontSize: 12,
+            fontWeight: FontWeight.w400),),
+      alignment: Alignment.bottomCenter,
+      autoCloseDuration: const Duration(seconds: 4),
+      primaryColor: Theme.of(context).colorScheme.primary,
+      foregroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      borderRadius: BorderRadius.circular(12.0),
+      direction: TextDirection.rtl,
+    );
+  }
+  static errorSnackBar(BuildContext context, String title, String decription) {
+    toastification.show(
+      context: context,
+      type: ToastificationType.error,
+      style: ToastificationStyle.flatColored,
+      title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: "dana",
+            color: ColorPallet.lightColorScheme.onSurface,
+            fontSize: 14,
+            fontWeight: FontWeight.w600),
+      ),
+      description: Text(decription,style: TextStyle(
+            fontFamily: "dana",
+            color: ColorPallet.lightColorScheme.onSurface,
+            fontSize: 12,
+            fontWeight: FontWeight.w400),),
+      alignment: Alignment.bottomCenter,
+      autoCloseDuration: const Duration(seconds: 4),
+      primaryColor: Theme.of(context).colorScheme.error,
+      foregroundColor: Theme.of(context).colorScheme.surface,
+      borderRadius: BorderRadius.circular(12.0),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      direction: TextDirection.rtl,
+    );
+  }
+}
