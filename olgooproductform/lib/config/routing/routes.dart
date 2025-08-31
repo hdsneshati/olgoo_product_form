692964b6cb0472e95f2fd61197ade64c7732c0c2
hdsneshati <hadisneshati1384@gmail.com>
2025-08-31 03:48:12 +0330

    add extra for setup4

--- olgooproductform/lib/config/routing/routes.dart
+++ olgooproductform/lib/config/routing/routes.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:olgooproductform/feature/domain/product/entity/product_entity.dart' show TempProduct;
 import 'package:olgooproductform/feature/presentation/authentication/bloc/auth/auth.bloc.dart';
 import 'package:olgooproductform/feature/presentation/authentication/screens/login.screen.dart';
 import 'package:olgooproductform/feature/presentation/authentication/screens/otp.screen.dart';
 import 'package:olgooproductform/feature/presentation/authentication/screens/signup.step1.screen.dart';
 import 'package:olgooproductform/feature/presentation/authentication/screens/signup_stap2_details_screen.dart';
-import 'package:olgooproductform/feature/presentation/dashboard/screens/dashboard_screen.dart';
-import 'package:olgooproductform/feature/presentation/dashboard/screens/signup_step3_product.dart';
-import 'package:olgooproductform/feature/presentation/dashboard/screens/signup_step4_order.dart';
+import 'package:olgooproductform/feature/presentation/product/screens/dashboard_screen.dart';
+import 'package:olgooproductform/feature/presentation/product/screens/signup_step3_product.dart';
+import 'package:olgooproductform/feature/presentation/product/screens/signup_step4_order.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -47,10 +48,15 @@ final GoRouter routs =
     builder: (context, state) => SignupStep3Product(),
   ),
    GoRoute(
-    path: '/signupstep4order',
-    name: "/signupstep4order",
-    builder: (context, state) => SignupStep4Order(),
-  ),
+  path: '/signupstep4order',
+  name: "signupstep4order", // بدون / در نام
+  builder: (context, state) {
+    // گرفتن اطلاعات محصول از صفحه قبل
+    final TempProduct tempProduct = state.extra as TempProduct;
+    return SignupStep4Order(tempProduct: tempProduct);
+  },
+),
+
    GoRoute(
     path: '/dashboard',
     name: "/dashboard",
